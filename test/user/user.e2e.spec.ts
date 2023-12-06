c349e3aea7d3b1c4dcc3fd2ddba2fe923dbd7f7d
Fahad Alhssan <fahadmajedh@gmail.com>
2023-12-06 13:06:28 +0300

    test: /register (POST)

--- test/user/user.e2e.spec.ts
+++ test/user/user.e2e.spec.ts
@@ -2,6 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
 import { JwtAuthGuard } from 'src/user/presentation/guards/jwt.guard';
+import { CreateUserRequest } from 'src/user/presentation/requests/create_user.request';
 import { UserController } from 'src/user/presentation/user.controller';
 import { UserModule } from 'src/user/user.module';
 
@@ -50,93 +51,158 @@ describe('Auth Service', () => {
       //? Then: A new Access token is provided
       expect(refreshResponse.body.accessToken).toBeDefined();
     });
+
+    test('should reject if no access token is provided', async () => {
+      //?Given: A registered user without providing an access token
+      const registerResponse = await controller.register({
+        email: 'fahad@gmail.com',
+        password: '123123',
+      });
+
+      //?When: Making a request to refresh endpoint without an access token
+      const refreshResponse = await request(app.getHttpServer())
+        .post('/users/refresh')
+
+        .send({ refreshToken: registerResponse.refreshToken });
+
+      //?Then: The response should indicate the absence of an access token
+      expect(refreshResponse.status).toBe(401);
+      expect(refreshResponse.body.message).toContain('jwt must be provided');
+    });
+
+    test('should reject if no refresh token is provided', async () => {
+      //?Given: A registered user without providing a refresh token
+      const registerResponse = await controller.register({
+        email: 'fahad@gmail.com',
+        password: '123123',
+      });
+
+      //?When: Making a request to refresh endpoint without a refresh token
+      const refreshResponse = await request(app.getHttpServer())
+        .post('/users/refresh')
+
+        .set('Authorization', `Bearer ${registerResponse.accessToken}`)
+        .send({});
+
+      //?Then: The response should indicate the absence of a refresh token
+      expect(refreshResponse.status).toBe(401);
+      expect(refreshResponse.body.message).toContain('jwt must be provided');
+    });
+
+    test('should reject if refresh token does not match user', async () => {
+      //?Given: A registered user and a mismatched refresh token
+      const registerResponse = await controller.register({
+        email: 'fahad@gmail.com',
+        password: '123123',
+      });
+
+      const registerResponse2 = await controller.register({
+        email: 'fahad2@gmail.com',
+        password: '123123',
+      });
+
+      const mismatchedRefreshToken = registerResponse2.refreshToken;
+
+      //?When: Making a request to refresh endpoint with a mismatched refresh token
+      const refreshResponse = await request(app.getHttpServer())
+        .post('/users/refresh')
+        .set('Authorization', `Bearer ${registerResponse.accessToken}`)
+
+        .send({ refreshToken: mismatchedRefreshToken });
+
+      //?Then: The response should indicate a mismatched token
+      expect(refreshResponse.status).toBe(401);
+      expect(refreshResponse.body.message).toContain(
+        "You Don't Own The Refresh Token",
+      );
+    });
+
+    test('should reject if refresh token is expired', async () => {
+      //? Given: A registered user with an expired refresh token
+      Date.now = jest.fn(() => new Date('2023-11-17T12:16:00Z').getTime());
+
+      const registerResponse = await controller.register({
+        email: 'fahad@gmail.com',
+        password: '123123',
+      });
+
+      Date.now = jest.fn(() => new Date('2223-11-17T12:16:00Z').getTime());
+
+      //? When: Making a request to refresh endpoint with an expired refresh toke
+      const refreshResponse = await request(app.getHttpServer())
+        .post('/users/refresh')
+        .set('Authorization', `Bearer ${registerResponse.accessToken}`)
+
+        .send({ refreshToken: registerResponse.refreshToken });
+
+      //? Then: The response should indicate token expiration
+      expect(refreshResponse.status).toBe(401);
+      expect(refreshResponse.body.message).toContain('jwt expired');
+    });
   });
 
-  test('should reject if no access token is provided', async () => {
-    //?Given: A registered user without providing an access token
-    const registerResponse = await controller.register({
-      email: 'fahad@gmail.com',
-      password: '123123',
+  describe('/register (POST)', () => {
+    test('should create new user', async () => {
+      const response = await controller.register(<CreateUserRequest>{
+        email: 'fahad@gmail.com',
+        firstName: 'Fahad',
+        lastName: 'Majed',
+        password: '213123',
+      });
+
+      expect(response.accessToken).toBeDefined();
+      expect(response.userID).toBe(1);
     });
 
-    //?When: Making a request to refresh endpoint without an access token
-    const refreshResponse = await request(app.getHttpServer())
-      .post('/users/refresh')
+    test('should not create new user if email exists', async () => {
+      await controller.register(<CreateUserRequest>{
+        email: 'fahad@gmail.com',
+        firstName: 'Fahad',
+        lastName: 'Majed',
+        password: '213123',
+      });
 
-      .send({ refreshToken: registerResponse.refreshToken });
-
-    //?Then: The response should indicate the absence of an access token
-    expect(refreshResponse.status).toBe(401);
-    expect(refreshResponse.body.message).toContain('jwt must be provided');
-  });
-
-  test('should reject if no refresh token is provided', async () => {
-    //?Given: A registered user without providing a refresh token
-    const registerResponse = await controller.register({
-      email: 'fahad@gmail.com',
-      password: '123123',
+      await expect(
+        controller.register(<CreateUserRequest>{
+          email: 'fahad@gmail.com',
+          firstName: 'Fahad',
+          lastName: 'Majed',
+          password: '213123',
+        }),
+      ).rejects.toThrow('This email address is already in use.');
     });
 
-    //?When: Making a request to refresh endpoint without a refresh token
-    const refreshResponse = await request(app.getHttpServer())
-      .post('/users/refresh')
+    test('should sign in', async () => {
+      await controller.register(<CreateUserRequest>{
+        email: 'fahad@gmail.com',
+        firstName: 'Fahad',
+        lastName: 'Majed',
+        password: '213123',
+      });
 
-      .set('Authorization', `Bearer ${registerResponse.accessToken}`)
-      .send({});
+      const response = await controller.signIn('fahad@gmail.com', '213123');
 
-    //?Then: The response should indicate the absence of a refresh token
-    expect(refreshResponse.status).toBe(401);
-    expect(refreshResponse.body.message).toContain('jwt must be provided');
-  });
-
-  test('should reject if refresh token does not match user', async () => {
-    //?Given: A registered user and a mismatched refresh token
-    const registerResponse = await controller.register({
-      email: 'fahad@gmail.com',
-      password: '123123',
+      expect(response.accessToken).toBeDefined();
+      expect(response.userID).toBeDefined();
     });
 
-    const registerResponse2 = await controller.register({
-      email: 'fahad2@gmail.com',
-      password: '123123',
+    test('should throw when signing in with wrong credentials', async () => {
+      await controller.register(<CreateUserRequest>{
+        email: 'fahad@gmail.com',
+        firstName: 'Fahad',
+        lastName: 'Majed',
+        password: '123123',
+      });
+
+      await expect(
+        controller.signIn('fahad@gmail.com', '213123'),
+      ).rejects.toThrow('Incorrect Password');
     });
 
-    const mismatchedRefreshToken = registerResponse2.refreshToken;
-
-    //?When: Making a request to refresh endpoint with a mismatched refresh token
-    const refreshResponse = await request(app.getHttpServer())
-      .post('/users/refresh')
-      .set('Authorization', `Bearer ${registerResponse.accessToken}`)
-
-      .send({ refreshToken: mismatchedRefreshToken });
-
-    //?Then: The response should indicate a mismatched token
-    expect(refreshResponse.status).toBe(401);
-    expect(refreshResponse.body.message).toContain(
-      "You Don't Own The Refresh Token",
-    );
-  });
-
-  test('should reject if refresh token is expired', async () => {
-    //? Given: A registered user with an expired refresh token
-    Date.now = jest.fn(() => new Date('2023-11-17T12:16:00Z').getTime());
-
-    const registerResponse = await controller.register({
-      email: 'fahad@gmail.com',
-      password: '123123',
+    test('should throw when signing in with no registration', async () => {
+      expect(controller.signIn('fahad@gmail.com', '213123')).rejects.toThrow(
+        'Email Not Found',
+      );
     });
-
-    Date.now = jest.fn(() => new Date('2223-11-17T12:16:00Z').getTime());
-
-    //? When: Making a request to refresh endpoint with an expired refresh toke
-    const refreshResponse = await request(app.getHttpServer())
-      .post('/users/refresh')
-      .set('Authorization', `Bearer ${registerResponse.accessToken}`)
-
-      .send({ refreshToken: registerResponse.refreshToken });
-
-    //? Then: The response should indicate token expiration
-    expect(refreshResponse.status).toBe(401);
-    expect(refreshResponse.body.message).toContain('jwt expired');
   });
 });
